e3cf89ace497120a81b9fa88564b62c7ef9a07b7
Felipe Martins <felipemjesus@outlook.com>
2019-09-20 22:08:34 -0300

    Criado o model e migration de Pessoa

---
+++ database/migrations/2019_09_21_010421_create_pessoas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('foto');
+            $table->string('email')->nullable();
+            $table->date('data_nascimento')->nullable();
+            $table->string('localidade');
+            $table->timestamps();
+
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
